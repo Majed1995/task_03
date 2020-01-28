9e60735e4d34d8d01852f5125fc46d611c6feabf
Majed1995 <majed.alattar@zain.com>
2020-01-28 12:06:08 +0300

    Task3.3

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,9 +4,21 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
-
+        "my_list" : [
+            {
+            'restaurant_name' : 'Tatami',
+            'food_type': 'Japanese',
+        },
+        {
+            'restaurant_name' : 'Texas Roadhouse ',
+            'food_type': 'American',
+        },
+        {
+            'restaurant_name' : 'Elevation Burger ',
+            'food_type': 'Burgers',
+        },
+        ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +26,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object" : 
+        {
+            "restaurant_name" : "Istanbul",
+            "food_type" : "Turkish"
+        }
     }
     return render(request, 'detail.html', context)
